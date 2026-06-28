4440edf55a3ff6b4194ae3ff6254f6877587b2d4
yoshimidevz <yoshimidev1@gmail.com>
2026-06-28 10:55:01 -0300

    feat: implementa padrao Result<T> generico para encapsular sucesso e falha entre camadas

---
+++ lib/core/result/result.dart
@@ -0,0 +1,51 @@
+sealed class Result<T> {
+  const Result();
+
+  const factory Result.success(T value) = Success<T>;
+
+  const factory Result.failure(Failure failure) = ResultFailure<T>;
+
+  bool get isSuccess => this is Success<T>;
+
+  bool get isFailure => this is ResultFailure<T>;
+
+  R when<R>({
+    required R Function(T value) onSuccess,
+    required R Function(Failure failure) onFailure,
+  }) {
+    return switch (this) {
+      Success<T>(value: final value) => onSuccess(value),
+      ResultFailure<T>(failure: final failure) => onFailure(failure),
+    };
+  }
+
+  T? get valueOrNull => switch (this) {
+        Success<T>(value: final value) => value,
+        ResultFailure<T>() => null,
+      };
+
+  Failure? get failureOrNull => switch (this) {
+        Success<T>() => null,
+        ResultFailure<T>(failure: final failure) => failure,
+      };
+}
+
+final class Success<T> extends Result<T> {
+  final T value;
+  const Success(this.value);
+}
+
+final class ResultFailure<T> extends Result<T> {
+  final Failure failure;
+  const ResultFailure(this.failure);
+}
+
+class Failure {
+  final String message;
+  final Object? exception;
+
+  const Failure(this.message, {this.exception});
+
+  @override
+  String toString() => 'Failure: $message';
+}
